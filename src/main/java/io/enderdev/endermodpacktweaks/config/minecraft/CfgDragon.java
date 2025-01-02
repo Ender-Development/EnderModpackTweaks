ba8824d0980baf017cae37d17d9751fe6a53bce7
MasterEnderman <xxmr.endermanxx@gmail.com>
2025-01-02 17:12:16 +0100

    overhauled dragon config

--- src/main/java/io/enderdev/endermodpacktweaks/config/minecraft/CfgDragon.java
+++ src/main/java/io/enderdev/endermodpacktweaks/config/minecraft/CfgDragon.java
@@ -3,7 +3,7 @@ package io.enderdev.endermodpacktweaks.config.minecraft;
 import net.minecraftforge.common.config.Config;
 
 public class CfgDragon {
-    @Config.Name("Enable Dragon Tweaks")
+    @Config.Name("[01] Enable Dragon Tweaks")
     @Config.Comment({
             "This tweak kills the first dragon when the player enters the end for the first time.",
             "Useful for modpacks that want to make the dragon fight non-free."
@@ -11,27 +11,31 @@ public class CfgDragon {
     @Config.RequiresMcRestart
     public boolean enable = false;
 
-    @Config.Name("Drop Dragon Egg")
+    @Config.Name("[02] Drop Dragon Egg")
     @Config.Comment("Should the auto killed dragon drop the dragon egg?")
     public boolean dropEgg = false;
 
-    @Config.Name("Multiple Dragon Egg")
-    @Config.Comment("Should every dragon drop an egg?")
-    public boolean multipleEgg = false;
+    @Config.Name("[03] Replace the first Dragon Egg")
+    @Config.Comment("Replace the dragon egg block with another block.")
+    public String eggBlock = "minecraft:dragon_egg";
 
-    @Config.Name("Create End Portal")
-    @Config.Comment("Should the auto killed dragon create the end portal?")
+    @Config.Name("[04] Create End Portal")
+    @Config.Comment("Should the auto killed dragon create the end portal back to the overworld?")
     public boolean createPortal = false;
 
-    @Config.Name("Create End Gateway")
+    @Config.Name("[05] Create End Gateway")
     @Config.Comment("Should the auto killed dragon create an end gateway?")
     public boolean createGateway = false;
 
-    @Config.Name("Disable End Portal")
+    @Config.Name("[06] Multiple Dragon Egg")
+    @Config.Comment("Should every dragon drop an egg?")
+    public boolean multipleEgg = false;
+
+    @Config.Name("[07] Disable End Portal")
     @Config.Comment("Should spawning the end portal when killing the dragon be disabled?")
     public boolean disablePortal = false;
 
-    @Config.Name("Disable End Gateway")
+    @Config.Name("[08] Disable End Gateway")
     @Config.Comment("Should spawning the end gateway when killing the dragon be disabled?")
     public boolean disableGateway = false;
 }
