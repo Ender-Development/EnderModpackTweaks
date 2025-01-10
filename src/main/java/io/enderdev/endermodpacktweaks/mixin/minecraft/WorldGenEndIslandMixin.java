62516b68444f542220e5980fad922cc961fbb013
MasterEnderman <xxmr.endermanxx@gmail.com>
2025-01-10 18:24:20 +0100

    nether api fix for end island replacement

--- src/main/java/io/enderdev/endermodpacktweaks/mixin/minecraft/WorldGenEndIslandMixin.java
+++ src/main/java/io/enderdev/endermodpacktweaks/mixin/minecraft/WorldGenEndIslandMixin.java
@@ -5,6 +5,7 @@ import io.enderdev.endermodpacktweaks.EMTConfig;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.gen.feature.WorldGenEndIsland;
+import net.minecraftforge.fml.common.Loader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -15,7 +16,7 @@ public class WorldGenEndIslandMixin {
     @Redirect(method = "generate", at = @At(value = "FIELD", target = "Lnet/minecraft/init/Blocks;END_STONE:Lnet/minecraft/block/Block;"))
     private Block replaceEndStone(@Local Block block) {
         Block block1 = Block.getBlockFromName(EMTConfig.MINECRAFT.END_ISLAND.endStone);
-        return block1 == null ? Blocks.END_STONE : block1;
+        return block1 == null || Loader.isModLoaded("nether_api")? Blocks.END_STONE : block1;
     }
 
     @ModifyVariable(method = "generate", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextInt(I)I", ordinal = 0))
