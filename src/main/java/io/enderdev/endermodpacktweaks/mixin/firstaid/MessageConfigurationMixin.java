f8094ed8b03c795a6b517326aea18f09597e268c
MasterEnderman <xxmr.endermanxx@gmail.com>
2025-01-12 10:03:48 +0100

    fix first aid tutorial message

---
+++ src/main/java/io/enderdev/endermodpacktweaks/mixin/firstaid/MessageConfigurationMixin.java
@@ -0,0 +1,15 @@
+package io.enderdev.endermodpacktweaks.mixin.firstaid;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import ichttt.mods.firstaid.common.network.MessageConfiguration;
+import io.enderdev.endermodpacktweaks.EMTConfig;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(value = MessageConfiguration.Handler.class, remap = false)
+public class MessageConfigurationMixin {
+    @ModifyExpressionValue(method = "lambda$onMessage$0", at = @At(value = "FIELD", target = "Lichttt/mods/firstaid/api/damagesystem/AbstractPlayerDamageModel;hasTutorial:Z", ordinal = 0))
+    private static boolean hasTutorial(boolean hasTutorial) {
+        return EMTConfig.FIRST_AID.disableTutorial || hasTutorial;
+    }
+}
