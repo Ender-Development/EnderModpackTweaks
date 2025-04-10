3abaf9edf0fb83d87cefd85a1dc19ff9e27d9f6c
MasterEnderman <xxmr.endermanxx@gmail.com>
2025-04-10 23:19:42 +0200

    added one more boss bar

--- src/main/java/io/enderdev/endermodpacktweaks/features/bossbar/ImprovedBossBarRenderer.java
+++ src/main/java/io/enderdev/endermodpacktweaks/features/bossbar/ImprovedBossBarRenderer.java
@@ -31,7 +31,7 @@ public class ImprovedBossBarRenderer extends Gui {
     public int getOverlayHeight(BossInfo info) {
         String mob = getEntityFromBossInfo(info);
         BossType boss = BossType.getBossType(mob);
-        return boss == null ? 0 : boss.overlayHeight;
+        return boss == null ? 0 : boss.getOverlayHeight();
     }
 
     public boolean hasOverlay(String text) {
@@ -50,9 +50,9 @@ public class ImprovedBossBarRenderer extends Gui {
         }
         ScaledResolution scaledresolution = new ScaledResolution(mc);
         GlStateManager.pushMatrix();
-        int middleX = (scaledresolution.getScaledWidth() / 2) - (boss.overlayWidth / 2);
-        renderBar(middleX + boss.barOffsetX, y + boss.barOffsetY, boss.barWidth, info, boss.bar);
-        renderOverlay(middleX, y, boss.overlay);
+        int middleX = (scaledresolution.getScaledWidth() / 2) - (boss.getBarWidth() / 2);
+        renderBar(middleX + boss.getBarOffsetX(), y + boss.getBarOffsetY(), boss.getBarWidth(), info, boss.getBar());
+        renderOverlay(middleX, y, boss.getOverlay());
         GlStateManager.popMatrix();
         return true;
     }
@@ -100,61 +100,5 @@ public class ImprovedBossBarRenderer extends Gui {
         private String id;
         private String type;
         private String name;
-    }
-
-
-    private enum BossType {
-        WITHER("minecraft:wither",
-                new ResourceLocation(Tags.MOD_ID, "textures/gui/boss_bars/wither.png"),
-                new ResourceLocation(Tags.MOD_ID, "textures/gui/bars/purple_progress.png"),
-                15, 10, 169, 189, 48),
-        ENDER_DRAGON("minecraft:ender_dragon",
-                new ResourceLocation(Tags.MOD_ID, "textures/gui/boss_bars/ender_dragon.png"),
-                new ResourceLocation(Tags.MOD_ID, "textures/gui/bars/pink_progress.png"),
-                15, 12, 162, 186, 41),
-        FERROUS_WROUGHTNAUT("mowziesmobs:ferrous_wroughtnaut",
-                new ResourceLocation(Tags.MOD_ID, "textures/gui/boss_bars/ferrous_wroughtnaut.png"),
-                new ResourceLocation(Tags.MOD_ID, "textures/gui/bars/red_progress.png"),
-                17, 11, 163, 190, 54),
-        FROSTMAW("mowziesmobs:frostmaw",
-                new ResourceLocation(Tags.MOD_ID, "textures/gui/boss_bars/frostmaw.png"),
-                new ResourceLocation(Tags.MOD_ID, "textures/gui/bars/ice_progress.png"),
-                13, 20, 150, 190, 49),
-        VOID_BLOSSOM("da:void_blossom",
-                new ResourceLocation(Tags.MOD_ID, "textures/gui/boss_bars/void_blossom.png"),
-                new ResourceLocation(Tags.MOD_ID, "textures/gui/bars/green_progress.png"),
-                16, 8, 169, 185, 47),
-        NIGHT_LICH("da:night_lich",
-                new ResourceLocation(Tags.MOD_ID, "textures/gui/boss_bars/night_lich.png"),
-                new ResourceLocation(Tags.MOD_ID, "textures/gui/bars/blue_progress.png"),
-                18, 22, 175, 219, 49),
-        GREAT_WYRK("da:great_wyrk",
-                new ResourceLocation(Tags.MOD_ID, "textures/gui/boss_bars/ancient_wyrk.png"),
-                new ResourceLocation(Tags.MOD_ID, "textures/gui/bars/ice_progress.png"),
-                15, 13, 172, 198, 50);
-
-        private final String entity;
-        private final ResourceLocation overlay;
-        private final ResourceLocation bar;
-        private final int barOffsetY;
-        private final int barOffsetX;
-        private final int barWidth;
-        private final int overlayWidth;
-        private final int overlayHeight;
-
-        BossType(String entity, ResourceLocation overlay, ResourceLocation bar, int barOffsetY, int barOffsetX, int barWidth, int overlayWidth, int overlayHeight) {
-            this.entity = entity;
-            this.overlay = overlay;
-            this.bar = bar;
-            this.barOffsetY = barOffsetY;
-            this.barOffsetX = barOffsetX;
-            this.barWidth = barWidth;
-            this.overlayWidth = overlayWidth;
-            this.overlayHeight = overlayHeight;
-        }
-
-        public static BossType getBossType(String entity) {
-            return Arrays.stream(values()).filter(bossType -> bossType.entity.equals(entity)).findFirst().orElse(null);
-        }
     }
 }
