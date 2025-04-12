79d2fa0f07d5726703fc18b2a5f01c800c516b33
tttsaurus <chenh1231y@gmail.com>
2025-04-12 15:35:09 -0400

    Roughly impl sliding text for recipe stage jei

--- src/main/java/io/enderdev/endermodpacktweaks/mixin/recipestages/StagedRecipeWrapperShapedMixin.java
+++ src/main/java/io/enderdev/endermodpacktweaks/mixin/recipestages/StagedRecipeWrapperShapedMixin.java
@@ -5,10 +5,14 @@ import com.blamejared.recipestages.recipes.RecipeStage;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import io.enderdev.endermodpacktweaks.config.CfgTweaks;
+import io.enderdev.endermodpacktweaks.utils.EmtRender;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.resources.I18n;
+import org.apache.commons.lang3.time.StopWatch;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.Locale;
@@ -18,12 +22,98 @@ public class StagedRecipeWrapperShapedMixin {
     @Shadow
     private RecipeStage recipe;
 
+    @Unique
+    private StopWatch enderModpackTweaks$stopWatch = null;
+
+    @Unique
+    private float enderModpackTweaks$deltaTime = 0f;
+
+    @Unique
+    private String enderModpackTweaks$localizedText = null;
+
+    @Unique
+    private float enderModpackTweaks$textWidth = 0f;
+
+    @Unique
+    private float enderModpackTweaks$xShift = 0f;
+
+    @Unique
+    private boolean enderModpackTweaks$needSliding;
+
+    @Unique
+    private float enderModpackTweaks$freezeTimer = 0f;
+
+    @Unique
+    private boolean enderModpackTweaks$onFreezeTiming = true;
+
+    public float rectWidth = 100f;
+    public float spareWidth = 20f;
+    public float xShiftSpeed = 15f;
+    public float freezeTime = 2f;
+    public boolean onDemandSliding = false;
+
     @WrapOperation(method = "drawInfo", at = @At(value = "INVOKE", target="Lnet/minecraft/client/gui/FontRenderer;drawString(Ljava/lang/String;III)I", remap = true), remap = false)
     public int drawString(FontRenderer instance, String text, int x, int y, int color, Operation<Integer> original) {
         if (!CfgTweaks.GAME_STAGES.localizeRecipeStages) {
             return original.call(instance, text, x + CfgTweaks.GAME_STAGES.recipeStagesTooltipXOffset, y + CfgTweaks.GAME_STAGES.recipeStagesTooltipYOffset, color);
         }
-        String localizedText = I18n.format("emt.game_stages." + recipe.getTier().toLowerCase(Locale.ROOT).trim());
-        return instance.drawString(I18n.format("gui.rs.tip.stage", localizedText), x + CfgTweaks.GAME_STAGES.recipeStagesTooltipXOffset, y + CfgTweaks.GAME_STAGES.recipeStagesTooltipYOffset, color);
+
+        if (enderModpackTweaks$stopWatch == null) {
+            enderModpackTweaks$stopWatch = new StopWatch();
+            enderModpackTweaks$stopWatch.start();
+        } else {
+            enderModpackTweaks$stopWatch.stop();
+            enderModpackTweaks$deltaTime = (float) (enderModpackTweaks$stopWatch.getNanoTime() / 1e9d);
+            enderModpackTweaks$stopWatch.reset();
+            enderModpackTweaks$stopWatch.start();
+        }
+
+        // <https://github.com/tttsaurus/Ingame-Info-Reborn/blob/master/src/main/java/com/tttsaurus/ingameinfo/common/impl/gui/control/SlidingText.java>
+        if ((!onDemandSliding || enderModpackTweaks$needSliding) && !enderModpackTweaks$onFreezeTiming) {
+            enderModpackTweaks$xShift += (enderModpackTweaks$deltaTime * xShiftSpeed);
+        }
+        if (enderModpackTweaks$onFreezeTiming) {
+            if (enderModpackTweaks$freezeTimer > freezeTime) {
+                enderModpackTweaks$freezeTimer = 0;
+                enderModpackTweaks$onFreezeTiming = false;
+            }
+            enderModpackTweaks$freezeTimer += enderModpackTweaks$deltaTime;
+        }
+
+        if (enderModpackTweaks$localizedText == null) {
+            enderModpackTweaks$localizedText = I18n.format("emt.game_stages." + recipe.getTier().toLowerCase(Locale.ROOT).trim());
+            enderModpackTweaks$localizedText = I18n.format("gui.rs.tip.stage", enderModpackTweaks$localizedText);
+            enderModpackTweaks$textWidth = Minecraft.getMinecraft().fontRenderer.getStringWidth(enderModpackTweaks$localizedText);
+            enderModpackTweaks$needSliding = enderModpackTweaks$textWidth > rectWidth;
+        }
+
+        x += CfgTweaks.GAME_STAGES.recipeStagesTooltipXOffset;
+        y += CfgTweaks.GAME_STAGES.recipeStagesTooltipYOffset;
+        if (!onDemandSliding || enderModpackTweaks$needSliding) {
+            EmtRender.prepareStencilToWrite(1);
+            EmtRender.drawRectStencilArea(x, y, rectWidth, Minecraft.getMinecraft().fontRenderer.FONT_HEIGHT);
+            EmtRender.prepareStencilToRender(1);
+
+            float firstX = x - enderModpackTweaks$xShift;
+            if (firstX > x - enderModpackTweaks$textWidth) {
+                EmtRender.renderText(enderModpackTweaks$localizedText, firstX, y, color);
+            }
+            if (enderModpackTweaks$xShift > spareWidth + enderModpackTweaks$textWidth - rectWidth) {
+                float secondX = x + spareWidth + enderModpackTweaks$textWidth - enderModpackTweaks$xShift;
+                if (secondX <= x) {
+                    enderModpackTweaks$xShift = 0;
+                    enderModpackTweaks$onFreezeTiming = true;
+                    EmtRender.renderText(enderModpackTweaks$localizedText, x, y, color);
+                } else {
+                    EmtRender.renderText(enderModpackTweaks$localizedText, secondX, y, color);
+                }
+            }
+
+            EmtRender.endStencil();
+        } else {
+            EmtRender.renderText(enderModpackTweaks$localizedText, x, y, 1f, color, true);
+        }
+
+        return 0;
     }
 }
--- src/main/java/io/enderdev/endermodpacktweaks/utils/EmtRender.java
+++ src/main/java/io/enderdev/endermodpacktweaks/utils/EmtRender.java
@@ -1,5 +1,7 @@
 package io.enderdev.endermodpacktweaks.utils;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
@@ -11,6 +13,7 @@ import java.awt.*;
 public class EmtRender {
     private static final Tessellator TESSELLATOR = Tessellator.getInstance();
     private static final BufferBuilder BUFFER_BUILDER = TESSELLATOR.getBuffer();
+    private static final FontRenderer FONT_RENDERER = Minecraft.getMinecraft().fontRenderer;
 
     public static void renderRect(float x, float y, float width, float height, Color color) {
         GlStateManager.pushMatrix();
@@ -32,7 +35,28 @@ public class EmtRender {
         GlStateManager.popMatrix();
     }
 
-    // <https://github.com/tttsaurus/Ingame-Info-Reborn/blob/4e79541b1f77ecf44f7d3718fd32555ca498d1cb/src/main/java/com/tttsaurus/ingameinfo/common/api/render/RenderUtils.java#L169>
+    public static void renderText(String text, float x, float y, int color) {
+        renderText(text, x, y, 1f, color, false);
+    }
+
+    // <https://github.com/tttsaurus/Ingame-Info-Reborn/blob/4e79541b1f77ecf44f7d3718fd32555ca498d1cb/src/main/java/com/tttsaurus/ingameinfo/common/api/render/RenderUtils.java>
+    public static void renderText(String text, float x, float y, float scale, int color, boolean shadow) {
+        GlStateManager.disableCull();
+        GlStateManager.enableTexture2D();
+        GlStateManager.disableLighting();
+        GlStateManager.enableBlend();
+        GlStateManager.disableAlpha();
+        GlStateManager.disableDepth();
+        GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(x, y, 0);
+        GlStateManager.scale(scale, scale, 0);
+        FONT_RENDERER.drawString(text, 0, 0, color, shadow);
+        GlStateManager.popMatrix();
+    }
+
+    // <https://github.com/tttsaurus/Ingame-Info-Reborn/blob/4e79541b1f77ecf44f7d3718fd32555ca498d1cb/src/main/java/com/tttsaurus/ingameinfo/common/api/render/RenderUtils.java>
     public static void renderRoundedRect(float x, float y, float width, float height, float radius, Color color) {
         int segments = Math.max(3, (int) (radius / 2f));
         float a = (float) color.getAlpha() / 255.0F;
@@ -40,21 +64,12 @@ public class EmtRender {
         float g = (float) color.getGreen() / 255.0F;
         float b = (float) color.getBlue() / 255.0F;
 
-        GlStateManager.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_FILL);
-        GL11.glEnable(GL11.GL_POLYGON_SMOOTH);
-
         GlStateManager.disableCull();
         GlStateManager.disableTexture2D();
         GlStateManager.enableBlend();
         GlStateManager.disableAlpha();
         GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
         GlStateManager.color(r, g, b, a);
-
-        GlStateManager.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_FILL);
-        GL11.glEnable(GL11.GL_POLYGON_SMOOTH);
-
-        GlStateManager.pushMatrix();
-        GlStateManager.translate(0, 0, 0);
 
         BUFFER_BUILDER.begin(GL11.GL_TRIANGLE_FAN, DefaultVertexFormats.POSITION);
 
@@ -79,11 +94,9 @@ public class EmtRender {
         BUFFER_BUILDER.pos(x + width - radius, y, 0).endVertex();
 
         TESSELLATOR.draw();
-
-        GlStateManager.popMatrix();
-        GL11.glDisable(GL11.GL_POLYGON_SMOOTH);
     }
 
+    // <https://github.com/tttsaurus/Ingame-Info-Reborn/blob/4e79541b1f77ecf44f7d3718fd32555ca498d1cb/src/main/java/com/tttsaurus/ingameinfo/common/api/render/RenderUtils.java>
     private static void addArcVertices(float cx, float cy, float radius, float startAngle, float endAngle, int segments) {
         startAngle -= 90;
         endAngle -= 90;
@@ -98,5 +111,49 @@ public class EmtRender {
             x = dx;
             y = dy;
         }
+    }
+
+    // <https://github.com/tttsaurus/Ingame-Info-Reborn/blob/4e79541b1f77ecf44f7d3718fd32555ca498d1cb/src/main/java/com/tttsaurus/ingameinfo/common/api/render/RenderUtils.java>
+    public static void prepareStencilToWrite(int stencilValue) {
+        if (!Minecraft.getMinecraft().getFramebuffer().isStencilEnabled())
+            Minecraft.getMinecraft().getFramebuffer().enableStencil();
+
+        GlStateManager.disableTexture2D();
+        GlStateManager.disableCull();
+
+        GL11.glEnable(GL11.GL_STENCIL_TEST);
+
+        GlStateManager.depthMask(false);
+        GlStateManager.colorMask(false, false, false, false);
+        GL11.glStencilFunc(GL11.GL_ALWAYS, stencilValue, 0xFF);
+        GL11.glStencilOp(GL11.GL_REPLACE, GL11.GL_REPLACE, GL11.GL_REPLACE);
+
+        // mask area
+        GL11.glStencilMask(0xFF);
+    }
+
+    // <https://github.com/tttsaurus/Ingame-Info-Reborn/blob/4e79541b1f77ecf44f7d3718fd32555ca498d1cb/src/main/java/com/tttsaurus/ingameinfo/common/api/render/RenderUtils.java>
+    public static void prepareStencilToRender(int stencilValue) {
+        GL11.glStencilMask(0x00);
+
+        GlStateManager.depthMask(true);
+        GlStateManager.colorMask(true, true, true, true);
+        GL11.glStencilFunc(GL11.GL_EQUAL, stencilValue, 0xFF);
+        GL11.glStencilOp(GL11.GL_KEEP, GL11.GL_KEEP, GL11.GL_KEEP);
+    }
+
+    // <https://github.com/tttsaurus/Ingame-Info-Reborn/blob/4e79541b1f77ecf44f7d3718fd32555ca498d1cb/src/main/java/com/tttsaurus/ingameinfo/common/api/render/RenderUtils.java>
+    public static void endStencil() {
+        GL11.glDisable(GL11.GL_STENCIL_TEST);
+    }
+
+    // <https://github.com/tttsaurus/Ingame-Info-Reborn/blob/4e79541b1f77ecf44f7d3718fd32555ca498d1cb/src/main/java/com/tttsaurus/ingameinfo/common/api/render/RenderUtils.java>
+    public static void drawRectStencilArea(float x, float y, float width, float height) {
+        BUFFER_BUILDER.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION);
+        BUFFER_BUILDER.pos(x, y, 0d).endVertex();
+        BUFFER_BUILDER.pos(x + width, y, 0d).endVertex();
+        BUFFER_BUILDER.pos(x + width, y + height, 0d).endVertex();
+        BUFFER_BUILDER.pos(x, y + height, 0d).endVertex();
+        TESSELLATOR.draw();
     }
 }
