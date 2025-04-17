05170f6a9e017958b7a1534309d9d4bcf448fad3
MasterEnderman <xxmr.endermanxx@gmail.com>
2025-04-17 14:31:37 +0200

    BPopener mixin now uses confighandler

--- src/main/java/io/enderdev/endermodpacktweaks/mixin/bpopener/BPOHandlerMixin.java
+++ src/main/java/io/enderdev/endermodpacktweaks/mixin/bpopener/BPOHandlerMixin.java
@@ -7,6 +7,8 @@ import io.bluebeaker.bpopener.BPOpenerConfig;
 import io.bluebeaker.bpopener.BPOpenerMod;
 import io.bluebeaker.bpopener.OpenAction;
 import io.enderdev.endermodpacktweaks.config.CfgTweaks;
+import io.enderdev.endermodpacktweaks.utils.EmtConfigHandler;
+import io.enderdev.endermodpacktweaks.utils.EmtConfigParser;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.gui.GuiScreen;
@@ -15,7 +17,6 @@ import net.minecraft.client.gui.inventory.GuiContainerCreative;
 import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Slot;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.text.TextComponentTranslation;
@@ -25,9 +26,6 @@ import org.lwjgl.input.Mouse;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
-
-import java.util.Arrays;
-import java.util.HashMap;
 
 
 @Mixin(value = BPOHandler.class, remap = false)
@@ -61,20 +59,10 @@ public abstract class BPOHandlerMixin {
     }
 
     @Unique
-    private static HashMap<Item, OpenAction> enderModpackTweaks$bpoMap = new HashMap<Item, OpenAction>() {
-        {
-            Arrays.asList(CfgTweaks.BP_OPENER.validItems).forEach(item -> {
-                String[] input = item.split(";");
-                if (input.length == 2) {
-                    Item item1 = Item.getByNameOrId(input[0]);
-                    if (item1 != null) {
-                        OpenAction action = Boolean.parseBoolean(input[1]) ? OpenAction.SNEAK_USE : OpenAction.USE;
-                        put(item1, action);
-                    }
-                }
-            });
-        }
-    };
+    private static final EmtConfigHandler<EmtConfigParser.ConfigItemWithBoolean> enderModpackTweaks$bpoConfig = new EmtConfigHandler<>(
+            CfgTweaks.BP_OPENER.validItems,
+            EmtConfigParser.ConfigItemWithBoolean::new
+    ).init();
 
     @WrapMethod(method = "onRightClick")
     private static void onRightClickWrap(GuiScreenEvent.MouseInputEvent.Pre event, Operation<Void> original) {
@@ -82,45 +70,45 @@ public abstract class BPOHandlerMixin {
             if (Mouse.getEventButtonState() && Mouse.getEventButton() == 1) {
                 GuiScreen screen = event.getGui();
                 if (screen instanceof GuiContainer) {
-                    GuiContainer container = (GuiContainer)screen;
+                    GuiContainer container = (GuiContainer) screen;
                     Slot slot = container.getSlotUnderMouse();
                     EntityPlayerSP player = mc.player;
                     if (slot != null && slot.inventory == player.inventory) {
                         if (isSlotValidToSwap(slot, screen)) {
                             ItemStack stack = slot.getStack();
                             if (stack != null && stack.getCount() <= 1) {
-                                OpenAction action = enderModpackTweaks$bpoMap.get(stack.getItem());
-                                if (action != null) {
-                                    lastSlot1 = slot.getSlotIndex();
-                                    lastSlot2 = player.inventory.currentItem;
-                                    if (BPOpenerConfig.debug) {
-                                        BPOpenerMod.getLogger().info("Attempt to swap slots {} slotNumber {} with hotbar {} in gui {}", slot.getSlotIndex(), slot.slotNumber, lastSlot2, container.getClass().getName());
+                                EmtConfigParser.ConfigItemWithBoolean configItem = (EmtConfigParser.ConfigItemWithBoolean) enderModpackTweaks$bpoConfig.get(stack);
+                                assert configItem != null;
+                                OpenAction action = configItem.value() ? OpenAction.SNEAK_USE : OpenAction.USE;
+                                lastSlot1 = slot.getSlotIndex();
+                                lastSlot2 = player.inventory.currentItem;
+                                if (BPOpenerConfig.debug) {
+                                    BPOpenerMod.getLogger().info("Attempt to swap slots {} slotNumber {} with hotbar {} in gui {}", slot.getSlotIndex(), slot.slotNumber, lastSlot2, container.getClass().getName());
+                                }
+
+                                int index1 = !(screen instanceof GuiInventory) && !(screen instanceof GuiContainerCreative) ? slot.slotNumber : slot.getSlotIndex();
+                                if (lastSlot1 < 9) {
+                                    player.inventory.currentItem = lastSlot1;
+                                } else {
+                                    doSwap(container.inventorySlots.windowId, index1, player.inventory.currentItem);
+                                }
+
+                                if (player.inventory.getCurrentItem() != stack) {
+                                    doSwap(container.inventorySlots.windowId, index1, player.inventory.currentItem);
+                                } else {
+                                    activated = true;
+                                    boolean shouldSneak = action.isSneaking();
+                                    previousSneaking = player.movementInput.sneak;
+                                    if (shouldSneak != previousSneaking) {
+                                        setPlayerSneakState(shouldSneak);
                                     }
 
-                                    int index1 = !(screen instanceof GuiInventory) && !(screen instanceof GuiContainerCreative) ? slot.slotNumber : slot.getSlotIndex();
-                                    if (lastSlot1 < 9) {
-                                        player.inventory.currentItem = lastSlot1;
-                                    } else {
-                                        doSwap(container.inventorySlots.windowId, index1, player.inventory.currentItem);
+                                    mc.playerController.processRightClick(player, mc.world, EnumHand.MAIN_HAND);
+                                    if (shouldSneak != previousSneaking) {
+                                        setPlayerSneakState(previousSneaking);
                                     }
 
-                                    if (player.inventory.getCurrentItem() != stack) {
-                                        doSwap(container.inventorySlots.windowId, index1, player.inventory.currentItem);
-                                    } else {
-                                        activated = true;
-                                        boolean shouldSneak = action.isSneaking();
-                                        previousSneaking = player.movementInput.sneak;
-                                        if (shouldSneak != previousSneaking) {
-                                            setPlayerSneakState(shouldSneak);
-                                        }
-
-                                        mc.playerController.processRightClick(player, mc.world, EnumHand.MAIN_HAND);
-                                        if (shouldSneak != previousSneaking) {
-                                            setPlayerSneakState(previousSneaking);
-                                        }
-
-                                        event.setCanceled(true);
-                                    }
+                                    event.setCanceled(true);
                                 }
                             }
                         }
@@ -138,10 +126,10 @@ public abstract class BPOHandlerMixin {
             if (stack != null && player != null) {
                 GuiScreen screen = mc.currentScreen;
                 if (screen instanceof GuiContainer) {
-                    Slot slot = ((GuiContainer)screen).getSlotUnderMouse();
+                    Slot slot = ((GuiContainer) screen).getSlotUnderMouse();
                     if (slot != null && slot.inventory == player.inventory) {
                         if (isSlotValidToSwap(slot, screen)) {
-                            if (enderModpackTweaks$bpoMap.get(stack.getItem()) != null) {
+                            if (enderModpackTweaks$bpoConfig.get(stack) != null) {
                                 event.getToolTip().add((new TextComponentTranslation("tooltip.bpopener.open.name")).getFormattedText());
                             }
                         }
