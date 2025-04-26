0d7d67ce024b22c1e0f9a4dd8917affe0c77b383
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2025-04-27 01:12:13 +0200

    rework auto jump to not use a tick event

--- src/main/java/io/enderdev/endermodpacktweaks/features/noautojump/AutoJumpHandler.java
+++ src/main/java/io/enderdev/endermodpacktweaks/features/noautojump/AutoJumpHandler.java
@@ -3,11 +3,11 @@ package io.enderdev.endermodpacktweaks.features.noautojump;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiControls;
+import net.minecraft.client.gui.GuiMainMenu;
 import net.minecraft.client.gui.GuiOptionButton;
 import net.minecraft.client.settings.GameSettings;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -15,19 +15,16 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class AutoJumpHandler {
     @SubscribeEvent
     public void onGuiInit(GuiScreenEvent.InitGuiEvent.Post event) {
-        if (event.getGui() instanceof GuiControls) {
+        if (event.getGui() instanceof GuiMainMenu) {
+            // doing it like this avoids having any extra event handlers and also ensures that any player won't see a disabled "Auto Jump: On" button
+            Minecraft.getMinecraft().gameSettings.autoJump = false;
+        } else if (event.getGui() instanceof GuiControls) {
             for (GuiButton button : event.getButtonList()) {
                 if (button instanceof GuiOptionButton && ((GuiOptionButton) button).getOption() == GameSettings.Options.AUTO_JUMP) {
                     button.enabled = false;
+                    break;
                 }
             }
-        }
-    }
-
-    @SubscribeEvent
-    public void onClientTick(TickEvent.ClientTickEvent event) {
-        if (event.phase == TickEvent.Phase.END) {
-            Minecraft.getMinecraft().gameSettings.autoJump = false;
         }
     }
 }
