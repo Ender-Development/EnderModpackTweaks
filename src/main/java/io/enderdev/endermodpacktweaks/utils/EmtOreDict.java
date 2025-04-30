42480c5b0c4ba7fd7e654f3f0ad4576ffd980da9
MasterEnderman <xxmr.endermanxx@gmail.com>
2025-04-30 10:46:04 +0200

    fix boss proof mixin

--- src/main/java/io/enderdev/endermodpacktweaks/utils/EmtOreDict.java
+++ src/main/java/io/enderdev/endermodpacktweaks/utils/EmtOreDict.java
@@ -4,16 +4,17 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 
+import java.util.Arrays;
+
 public final class EmtOreDict {
     public static final String PROOF_ENDER_DRAGON = "proofEnderDragon";
     public static final String PROOF_WITHER = "proofWither";
 
     public static boolean hasOreDictionary(IBlockState state, String oreDict) {
-        int id = OreDictionary.getOreID(oreDict);
         try {
-            for (int i : OreDictionary.getOreIDs(new ItemStack(state.getBlock(), 1, state.getBlock().damageDropped(state))))
-                if (i == id) return true;
-        } catch (Exception ignored) {}
+            return Arrays.stream(OreDictionary.getOreIDs(new ItemStack(state.getBlock(), 1, state.getBlock().damageDropped(state)))).anyMatch(i -> i == OreDictionary.getOreID(oreDict));
+        } catch (Exception ignore) {
+        }
         return false;
     }
 }
