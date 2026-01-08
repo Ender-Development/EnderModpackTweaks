d052a162a4129888d57577f3d5858da947add1e6
rozbrajaczpoziomow <roz@rozbrajacz.futbol>
2026-01-08 10:02:29 +0100

    use @WrapOperation instead of @Redirect

--- src/main/java/io/enderdev/endermodpacktweaks/mixin/biometweaker/BiomeTweakerMixin.java
+++ src/main/java/io/enderdev/endermodpacktweaks/mixin/biometweaker/BiomeTweakerMixin.java
@@ -1,18 +1,19 @@
 package io.enderdev.endermodpacktweaks.mixin.biometweaker;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import me.superckl.biometweaker.BiomeTweaker;
 import org.apache.commons.io.FileUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.io.File;
 import java.io.FilenameFilter;
 
 @Mixin(value = BiomeTweaker.class, remap = false)
 public class BiomeTweakerMixin {
-	@Redirect(method = "parseScripts", at = @At(value = "INVOKE", target = "Ljava/io/File;listFiles(Ljava/io/FilenameFilter;)[Ljava/io/File;"))
-	public File[] listFiles(File instance, FilenameFilter filenameFilter) {
+	@WrapOperation(method = "parseScripts", at = @At(value = "INVOKE", target = "Ljava/io/File;listFiles(Ljava/io/FilenameFilter;)[Ljava/io/File;"))
+	public File[] listFiles(File instance, FilenameFilter filenameFilter, Operation<File[]> original) {
 		return FileUtils.listFiles(instance, new String[] {"cfg"}, true).toArray(new File[0]);
 	}
 }
